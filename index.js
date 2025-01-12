c1743941d5faff1c6bedcea08363da20243290a0
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-12 18:51:25 +0000

    Create win logic

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ let firstCard = 7;
 let secondCard = 9;
 
 let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Congratulations! You've got blackjack")
+} else {
+    console.log("Unlucky you've bust!")
+}
