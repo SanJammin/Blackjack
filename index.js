d90f2bd7d68a993e564cf3975e5969552feaf8ec
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-13 21:47:18 +0000

    Create startGame function

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum < 21) {
-    message = "Hit or stick?";
-} else if (sum === 21) {
-    message = "Congratulations! You've got blackjack";
-    hasBlackJack = true;
-} else {
-    message = "Unlucky you've bust!";
-    isAlive = false;
+function startGame() {
+    if (sum < 21) {
+        message = "Hit or stick?";
+    } else if (sum === 21) {
+        message = "Congratulations! You've got blackjack";
+        hasBlackJack = true;
+    } else {
+        message = "Unlucky you've bust!";
+        isAlive = false;
+    }
 }
+
